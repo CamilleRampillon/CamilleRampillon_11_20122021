54e6cdd109fcc467f8fcb94be157e5e1137f2340
CamilleRampillon <camille.rampillon@gmail.com>
2021-12-29 10:52:20 +0100

    app Kasa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import Accomodation from './pages/Accomodation'
+import Error from './pages/Error'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+/**
+ * Manages routes & renders pages
+ * Gets the data for the site & passes it to the components
+ * @function App
+ * @returns {bollean} isError: 
+ * @returns{boolean} isLoaded: 
+ * @returns {array} kasaPlaces: all the accomodation information
+ * @returns {JSX}
+ */
+class App extends Component {
+// FETCH() data to initialise the app
+  state = {
+    places :[],
+    isLoaded: false,
+    error: false
+  }
+
+    componentDidMount() {
+      fetch("/logements.json")
+      .then(res=> res.json())
+          .then(datas =>{
+              this.setState({
+                places: datas,
+                isLoaded : true
+              });
+          },error=>{
+              this.setState({error})
+    })
+}
+
+render()
+
+  {  
+    return (
+    <Router>
+      <Header />   
+        <Switch>
+          <Route path="/" exact component={()=> <Home kasaPlaces={this.state.places} isLoaded={this.state.isLoaded} isError={this.state.error}/>}/>
+          <Route path="/about" exact component={About} />
+          <Route path="/accomodation/:id" exact component={(props)=><Accomodation kasaPlaces={this.state.places} isLoaded={this.state.isLoaded} isError={this.state.error} {...props}/>} />
+          <Route component={Error} />
+        </Switch> 
+      <Footer />
+    </Router>  
+    )
+  }   
 }
 
 export default App;
